00059d16e37ff41a990d63ce3af4b4027e13697b
Marin Vitali <90marinvitalii@gmail.com>
2023-06-19 22:47:35 +0600

    Доработал задание task_2.py

--- Homework11/task_2.py
+++ Homework11/task_2.py
@@ -47,9 +47,119 @@ try:
     assert contacts.text == contacts_txt
     assert contacts.get_attribute("innerText") == contacts_txt
 
-    print('Перейти в реестр "Контакты"')
+    print('Клик на кнопку "Контакты"')
+    sleep(1)
     contact = driver.find_element(By.CSS_SELECTOR, '[data-qa="Контакты"]')
-    sleep(5)
-    contact.click()
+    action_chains = ActionChains(driver)
+    action_chains.click(contact).perform()
+
+    print('Перейти в реестр "Контакты"')
+    sleep(2)
+    contacts = driver.find_element(By.CSS_SELECTOR, '[data-qa="Контакты"]')
+    contacts.click()
+
+    print('Проверить адрес сайта и заголовок страницы')
+    sleep(2)
+    assert 'fix-online.sbis.ru' in driver.current_url
+    assert driver.title == 'Контакты'
+
+    print('Проверить отображение трех вкладок')
+    tabs = driver.find_elements(By.CSS_SELECTOR, '.controls-Tabs__item')
+    assert len(tabs) == 3
+
+    print('Проверить текст, атрибут и видимость вкладки "Диалоги"')
+    sleep(2)
+    tab_contacts_txt = 'Диалоги'
+    tab_contacts = driver.find_element(By.CSS_SELECTOR, '[title="Диалоги"]')
+    assert tab_contacts.text == tab_contacts_txt
+    assert tab_contacts.get_attribute("innerText") == tab_contacts_txt
+    assert tab_contacts.is_displayed(), 'Элемент не отображается'
+
+    print('Клик на вкладку "Диалоги"')
+    tab_contacts.click()
+
+    print('Проверить атрибут и видимость кнопки "+"')
+    sleep(1)
+    contacts = driver.find_element(By.CSS_SELECTOR, '[data-qa="sabyPage-addButton"]')
+    assert contacts.get_attribute("tabindex") == "0"
+    assert contacts.is_displayed(), 'Элемент не отображается'
+
+    print('Клик на кнопку "+"')
+    contacts.click()
+
+    print('Проверить видимость строки поиска')
+    sleep(3)
+    search_line = driver.find_element(By.CSS_SELECTOR, '[data-qa="addressee-selector-root"] [data-qa="controls-Render__field"]')
+    assert search_line.is_displayed(), 'Элемент не отображается'
+
+    print('Клик в строку поиска')
+    search_line.click()
+
+    print('Ввести ФИО')
+    sleep(2)
+    name = "О'Нил Шакил"
+    fio = driver.find_element(By.CSS_SELECTOR, '.controls-Search__nativeField_caretFilled')
+    fio.send_keys(name)
+    assert fio.get_attribute('value') == name
+
+    print('Проверить текст, атрибут и видимость сотрудника')
+    sleep(1)
+    employee_txt = "О'Нил Шакил"
+    employee = driver.find_element(By.CSS_SELECTOR, '[data-qa="person-Information__fio"]')
+    assert employee.text == employee_txt
+    assert employee.get_attribute("tabindex") == "0"
+    assert employee.is_displayed(), 'Элемент не отображается'
+
+    print('Клик на сотрудника')
+    employee.click()
+
+    print('Проверить атрибут и видимость поля ввода')
+    sleep(2)
+    entry_field = driver.find_element(By.CSS_SELECTOR, '[data-qa="textEditor_slate_Field"]')
+    assert entry_field.get_attribute("role") == "textbox"
+    assert entry_field.is_displayed(), 'Элемент не отображается'
+
+    print('Ввести текст сообщения')
+    sleep(1)
+    message = 'Привет'
+    msg = driver.find_element(By.CSS_SELECTOR, '[data-qa="textEditor_slate_Field"]')
+    msg.send_keys(message)
+    assert msg.get_attribute('innerText') == message
+
+    print('Проверить атрибут и видимость кнопки "Отправить"')
+    sleep(1)
+    send_button = driver.find_element(By.CSS_SELECTOR, '[data-qa="msg-send-editor__send-button"]')
+    assert send_button.get_attribute("tabindex") == "-1"
+    assert send_button.is_displayed(), 'Элемент не отображается'
+
+    print('Клик на кнопку "Отправить"')
+    send_button.click()
+
+    print('Проверить текст, атрибут и видимость созданного сообщения')
+    sleep(1)
+    mesg_txt = "Привет"
+    mesg = driver.find_element(By.CSS_SELECTOR, '.msg-entity-text')
+    assert mesg.text == mesg_txt
+    assert mesg.get_attribute("tabindex") == "0"
+    assert mesg.is_displayed(), 'Элемент не отображается'
+
+    print('Навести курсор на сообщение и открыть')
+    sleep(3)
+    ms = driver.find_element(By.CSS_SELECTOR, '.msg-dialogs-item')
+    action_chains = ActionChains(driver)
+    action_chains.click(ms).perform()
+
+    print('Проверить видимость кнопки "Удалить"')
+    sleep(3)
+    delete = driver.find_element(By.CSS_SELECTOR, '[data-qa = "remove"]')
+    assert delete.is_displayed(), 'Элемент не отображается'
+
+    print('Клик на кнопку "Удалить"')
+    delete.click()
+
+    print('Проверить отображение удаленного сообщения')
+    sleep(3)
+    mss = driver.find_elements(By.CSS_SELECTOR, '.msg-dialogs-item')
+    assert len(mss) == 0, 'Удаленное сообщение отображается'
 finally:
     driver.quit()
